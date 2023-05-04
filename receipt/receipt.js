73ee00e7297c602bfcd106ee309173a4c25b8e97
Ammar Jahangeer <ammar11952@gmail.com>
2023-05-04 15:43:14 +0500

    Feature Receipt get template

---
+++ receipt/receipt.js
@@ -0,0 +1,7 @@
+const returnReceipt = (req, res) => {
+  //   res.send('RECEIPT CALLED');
+  //   console.log('receipt JS called');
+  res.send('Under Construction');
+};
+
+module.exports = returnReceipt;
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ router.use('/user', require('./user.routes'));
 router.use('/beneficiary', require('./beneficiary.routes'));
 router.use('/account', require('./account.routes'));
 router.use('/transaction', require('./transaction.routes'));
+
 router.get(
   '/receipt/get',
   require('../auth.middleware'),
