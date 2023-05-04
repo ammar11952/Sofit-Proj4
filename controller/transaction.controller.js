73e24aaffd719e152a03c42e487b5347dd928f67
Ammar Jahangeer <ammar11952@gmail.com>
2023-05-04 14:34:05 +0500

    Feat Transaction complete

--- controller/transaction.controller.js
+++ controller/transaction.controller.js
@@ -1,5 +1,6 @@
 const Transaction = require('../db/transaction');
 const Account = require('../db/account');
+const { error } = require('console');
 
 const postTransactionController = async (req, res) => {
   //   console.log('PostTransactionController');
@@ -9,36 +10,42 @@ const postTransactionController = async (req, res) => {
     })
     .catch((err) => {
       //   console.log('getAccount Error: ', err.message);
-      res.status(400).send('Fetch Account Details Error :', err.message);
+      res.status(404).send('Fetch Account Details Error :', err.message);
       return null;
     });
+
   if (senderBalance >= req.body.amount) {
-    const postTransaction = Transaction.create(req.body)
+    senderBalance -= req.body.amount;
+    Account.findOneAndUpdate(
+      { _id: req.body.senderAccId },
+      { balance: senderBalance }
+    )
       .then((doc) => {
-        console.log('Successful: postTransaction');
-        senderBalance -= req.body.amount;
-        return Account.findOneAndUpdate(
-          { _id: req.body.senderAccId },
-          { balance: senderBalance }
-        );
-      })
-      .then((doc) => {
-        res.status(200).send('Transaction Successful: Balance Updated');
-      })
-      .catch((err) => {
-        console.log('postTransaction Error: ', err.message);
-        res
-          .status(400)
-          .send(
-            'UNUPDATED BALANCE AFTER TRANSACTION: PLEASE REVIEW',
-            err.message
+        //balance updated transaction post pending
+        Transaction.create({ status: 'Success', ...req.body })
+          .then((doc) => res.status(200).send(doc))
+          .catch((err) =>
+            res
+              .status(400)
+              .send(
+                'Please Review. Balance updated but transaction not added:',
+                err.message
+              )
           );
-      });
+      })
+      .catch((err) =>
+        res
+          .status(200)
+          .send('Balance Update Failed: Transaction not created!', err.message)
+      );
   } else res.status(400).send('Insufficuent Balance in sender Account!');
 };
 
 const getTransactionController = async (req, res) => {
-  const getTransaction = Transaction.find({ senderAccId: req.mwAuthUserId })
+  const getTransaction = Transaction.find({
+    senderAccId: req.body.senderAccId,
+    receiverAccId: req.body.receiverAccId,
+  })
     .then((doc) => {
       console.log('Successful: getTransaction');
       res.status(200).send(doc);
